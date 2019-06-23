3794ad1e4d8498e320a9861f08a6b40b7ac8c48f
Chris Cheng <chris38c28@hotmail.com>
2019-06-23 15:44:43 +0800

    Add eslint & postcss

--- src/script/gdqjsongen.js
+++ src/script/gdqjsongen.js
@@ -1,33 +1,32 @@
-var request = require('request');
-var fs = require('fs');
+const request = require('request');
+const fs = require('fs');
 
-var gdqrun = {
-    url:'https://gamesdonequick.com/tracker/search/?type=run&event=26',
-    method: 'GET',
-    json:true
-}
-request(gdqrun, function (error, response, body) {
-    let output={};
-    body.forEach((element)=>{
-        output[element.pk]={
-            "tw": element.fields.name,
-            "sr": "",
-            "vod": "",
-            "yt": "",
-            "gm": "",
-        }
-    });
-    output['logo'] = "";
-    output['yetee_link'] = "";
-    output['gdq_link'] = "";
-    let data = JSON.stringify(output, null, 4);
+const gdqrun = {
+  url: 'https://gamesdonequick.com/tracker/search/?type=run&event=26',
+  method: 'GET',
+  json: true,
+};
+request(gdqrun, (error, response, body) => {
+  const output = {};
+  body.forEach((element) => {
+    output[element.pk] = {
+      tw: element.fields.name,
+      sr: '',
+      vod: '',
+      yt: '',
+      gm: '',
+    };
+  });
+  output.logo = '';
+  output.yetee_link = '';
+  output.gdq_link = '';
+  const data = JSON.stringify(output, null, 4);
 
-    fs.writeFile('tw.json', data, (err) => {  
-        // throws an error, you could also catch it here
-        if (err) throw err;
-    
-        // success case, the file was saved
-        console.log('saved!');
-    });
+  fs.writeFile('tw.json', data, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('saved!');
+  });
 });
-
