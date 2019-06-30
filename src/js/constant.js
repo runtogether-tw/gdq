e31f02e9ce5c8f9684b8f44713db12b0c970ec17
Chris Cheng <chris38c28@hotmail.com>
2019-07-01 01:20:32 +0800

    Add past events json files

--- src/js/constant.js
+++ src/js/constant.js
@@ -1,5 +1,11 @@
 export const LATEST_EVENT = 26;
 export const EVENT_LIST = {
+  18: 'SGDQ2016',
+  19: 'AGDQ2016',
+  20: 'SGDQ2017',
+  22: 'AGDQ2018',
+  23: 'SGDQ2018',
+  24: 'GDQX2018',
   25: 'AGDQ2019',
   26: 'SGDQ2019',
 };
